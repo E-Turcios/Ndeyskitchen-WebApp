faaa963599907d8b43b974f88bb24d6c0312c928
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-10-10 01:32:45 -0400

    Feat: Git acting idk why

--- server/server.js
+++
@@ -1,9 +0,0 @@
-const app = require('./index');
-
-const DBConnection = require('./database/DBConnect.js');
-
-if (DBConnection) {
-  app.listen(process.env.PORT, () => {
-    console.log('Server is listenning on port', process.env.PORT);
-  });
-}
