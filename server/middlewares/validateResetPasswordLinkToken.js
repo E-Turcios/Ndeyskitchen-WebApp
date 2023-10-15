fb8b2d0fb143f5de9eab359bd2c9267e23f3cff8
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-10-14 23:01:41 -0400

    Feat: Reset password Link test done

--- server/middlewares/validateResetPasswordLinkToken.js
+++ server/middlewares/validateResetPasswordLinkToken.js
@@ -1,17 +1,16 @@
 const User = require('../database/models/userModel');
 const jwt = require('jsonwebtoken');
 
-const { TOKEN_EXPIRED } = require('../messages');
+const { UNAUTHORIZED_REQUEST, TOKEN_EXPIRED } = require('../messages');
 
 async function validateResetPasswordLinkToken(req, res, next) {
   const { userToken } = req.body;
   console.log(userToken);
 
   try {
-    jwt.verify(userToken, process.env.JWT, async (err, data) => {
-      const payload = jwt.verify(userToken, process.env.JWT, {
-        ignoreExpiration: true,
-      });
+    jwt.verify(userToken, process.env.JWT, async (err, payload) => {
+      if (err && err.name === 'JsonWebTokenError')
+        return res.status(401).json({ Message: UNAUTHORIZED_REQUEST });
 
       if (err === null) {
         const user = await User.findOne({
@@ -26,7 +25,6 @@ async function validateResetPasswordLinkToken(req, res, next) {
 
       if (err && err.name === 'TokenExpiredError') {
         await User.findByIdAndUpdate(payload.id, { token: '' });
-        console.log(payload.id);
         return res.status(401).json({ Message: TOKEN_EXPIRED });
       }
     });
