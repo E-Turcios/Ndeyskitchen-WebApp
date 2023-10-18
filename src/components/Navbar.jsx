057b58458a4bb426b76bf41f6fa612dd1e049249
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-10-18 05:55:57 -0400

    Feat: Navbar layout readjusted

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,33 +4,28 @@ import Image from '../components/Image';
 export default function Navbar() {
   return (
     <div className="navbar-container">
-      <div className="logo-img-container">
+      <div className="navbar-start">
         <a href="/">
-          {' '}
           <Image className="logo-img" src={'ndeys-kitchen.png'} />
         </a>
+
+        <div className="navbar-information">
+          <a>Menu</a>
+          <a>Order</a>
+        </div>
       </div>
 
-      <div className="navbar-information">
-        <a>Menu</a>
-        <a>Order</a>
+      <div className="navbar-search-bar-container">
+        <input placeholder="Search our menu" className="search-bar" />
       </div>
-
-      <div className="navbar-elements-div">
-        <div className="navbar-search-bar-container">
-          <input placeholder="Search our menu" className="search-bar" />
-        </div>
-        <div className="navbar-icons-container">
-          <span class="material-symbols-outlined navbar-icons">
-            shopping_bag
-          </span>
-          <span class="material-symbols-outlined navbar-icons">person</span>
-          <a href="/login">
-            <button type="button" className="form-btn">
-              Login
-            </button>
-          </a>
-        </div>
+      <div className="navbar-icons-container">
+        <span class="material-symbols-outlined navbar-icons">shopping_bag</span>
+        <span class="material-symbols-outlined navbar-icons">person</span>
+        <a href="/login">
+          <button type="button" className="form-btn">
+            Login
+          </button>
+        </a>
       </div>
     </div>
   );
