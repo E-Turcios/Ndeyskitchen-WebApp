1b57827e2664ca1f1f997ce515e9fc4009c41d9d
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-10-31 19:21:29 -0400

    Feat: Search Bar onclick out

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,16 +1,29 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import SearchResult from './SearchResult';
 import searchResultStore from '../stores/searchResultStore';
+import useOnclickOutside from 'react-cool-onclickoutside';
 
 export default function SearchBar() {
   const [isSearchIconClicked, setIsSearchIconClicked] = useState(false);
   const [searchText, setSearchText] = useState('');
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    setOpen(searchText !== '');
+  }, [searchText]);
+
+  console.log(open);
+
+  const ref = useOnclickOutside(() => {
+    setIsSearchIconClicked(false);
+    setOpen(false);
+  });
 
   if (searchText === '') searchResultStore.setResult('');
 
   return (
     <>
-      <div className="search-field">
+      <div className="search-field" ref={ref}>
         <div
           className="navbar-search-bar-container"
           style={{ border: !isSearchIconClicked ? 'none' : null }}
@@ -36,14 +49,14 @@ export default function SearchBar() {
 
           <span
             style={{ border: !isSearchIconClicked ? 'none' : null }}
-            onClick={() => setIsSearchIconClicked(!isSearchIconClicked)}
+            onClick={() => setIsSearchIconClicked(true)}
             className="material-symbols-outlined navbar-icons"
           >
             search
           </span>
         </div>
 
-        <SearchResult open={searchText !== ''} searchText={searchText} />
+        <SearchResult open={open} searchText={searchText} />
       </div>
     </>
   );
--- client/src/components/SearchResult.jsx
+++ client/src/components/SearchResult.jsx
@@ -5,25 +5,25 @@ import searchResultStore from '../stores/searchResultStore';
 export default function SearchResult({ open, searchText }) {
   const { items, isLoading } = useFetchedItems();
 
-  if (!open) return null;
-
   const filteredItems = items.filter(item =>
     item.name.toLowerCase().includes(searchText.toLowerCase())
   );
 
   return (
-    <div className="search-result">
-      {isLoading ? (
-        <p>Searching...</p>
-      ) : filteredItems.length === 0 ? (
-        <p>No result.</p>
-      ) : (
-        filteredItems.map((item, id) => (
-          <p key={id} onClick={() => searchResultStore.setResult(item.name)}>
-            {item.name}
-          </p>
-        ))
-      )}
-    </div>
+    open && (
+      <div className="search-result">
+        {isLoading ? (
+          <p>Searching...</p>
+        ) : filteredItems.length === 0 ? (
+          <p>No result.</p>
+        ) : (
+          filteredItems.map((item, id) => (
+            <p key={id} onClick={() => searchResultStore.setResult(item.name)}>
+              {item.name}
+            </p>
+          ))
+        )}
+      </div>
+    )
   );
 }
