355f02eb8b98837c9d1a71503da6cfbb2583e14f
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-03 21:36:12 -0400

    Feat: link set to card container

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -12,24 +12,26 @@ export default function ItemCard(props) {
 
   return (
     <div className="item-container">
-      <div className="item-card-container">
-        <div className="item-image-container">
-          <Image alt={props.alt} className="item-image" src={props.src} />
+      <a
+        href={`/item/${props.id}`}
+        style={{ textDecoration: 'none', color: '#000' }}
+      >
+        <div className="item-card-container">
+          <div className="item-image-container">
+            <Image alt={props.alt} className="item-image" src={props.src} />
+          </div>
+          <div className="item-price-container">
+            <p>D {props.price}</p>
+          </div>
+          <p className="item-name">{props.name}</p>
+          <div className="item-component-container">
+            <i>{truncateComponentList(props.components)}</i>
+          </div>
         </div>
-        <div className="item-price-container">
-          <p>D {props.price}</p>
-        </div>
-        <p className="item-name">{props.name}</p>
-        <div className="item-component-container">
-          <i>{truncateComponentList(props.components)}</i>
-        </div>
-
-        <a href={`/item/${props.id}`} target="_blank">
-          <button className="buy-item-btn">
-            <span className="material-symbols-outlined">shopping_cart</span>
-          </button>
-        </a>
-      </div>
+      </a>
+      <button className="buy-item-btn">
+        <span className="material-symbols-outlined">shopping_cart</span>
+      </button>
     </div>
   );
 }
