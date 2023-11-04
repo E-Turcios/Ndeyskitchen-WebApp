1666a78aae77ff43576152b6c2aab079dd6335cb
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-04 17:10:42 -0400

    Feat: Refactoring

--- client/src/components/DateAndTime.jsx
+++ client/src/components/DateAndTime.jsx
@@ -1,36 +1,11 @@
 import { React, useState } from 'react';
+import dateAndTime from '../scripts/dateAndTime';
 
 export default function DateAndTime({ item }) {
   const [date, setDate] = useState('');
   const [time, setTime] = useState('');
 
-  let minDay = new Date().getDate().toString();
-  let maxDay = (new Date().getDate() + 1).toString();
-
-  let minMonth = (new Date().getMonth() + 1).toString();
-  let maxMonth = (new Date().getMonth() + 1).toString();
-
-  let minYear = new Date().getFullYear().toString();
-  let maxYear = new Date().getFullYear().toString();
-
-  if (item.filter === 'Cakes') {
-    minDay = (new Date().getDate() + 3).toString();
-    maxMonth = ((new Date().getMonth() + 4) % 12).toString();
-  }
-
-  if (minDay.length === 1) minDay = '0' + minDay;
-  if (maxDay.length === 1) maxDay = '0' + maxDay;
-
-  if (minMonth.length === 1) minMonth = '0' + minMonth;
-  if (maxMonth.length === 1) maxMonth = '0' + maxMonth;
-
-  if (parseInt(maxMonth) < parseInt(minMonth))
-    maxYear = (new Date().getFullYear() + 1).toString();
-
-  const minimumDate = `${minYear + `-` + minMonth + `-` + minDay}`;
-  const maximumDate = `${maxYear + `-` + maxMonth + `-` + maxDay}`;
-
-  console.log(maximumDate);
+  const { minimumDate, maximumDate } = dateAndTime({ item });
 
   return (
     <div>
