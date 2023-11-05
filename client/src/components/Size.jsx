8dd59aea9d983e8a550f13b463f7863c999a66f3
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-05 17:06:58 -0500

    Feat: Size collection

--- client/src/components/Size.jsx
+++ client/src/components/Size.jsx
@@ -1,13 +1,20 @@
 import { React, useState } from 'react';
 import { itemOptions } from '../scripts/itemOptions.js';
 
-export default function Size({ item, handleSizeSelect }) {
+export default function Size({
+  item,
+  handleSizeSelect,
+  handleSizeSelectPrice,
+}) {
   const viewItem = itemOptions.find(viewItem => viewItem.name === item.name);
   const [price, setPrice] = useState(item.price);
+  const [itemSize, setSize] = useState(item.size);
 
   return (
     <>
       <p className="tag">Size</p>
+      {handleSizeSelectPrice(price)}
+
       <div className="size-container">
         {Object.keys(viewItem.size).map(size => (
           <div
@@ -19,9 +26,10 @@ export default function Size({ item, handleSizeSelect }) {
             }
             onClick={() => {
               setPrice(viewItem.size[size]);
-              handleSizeSelect(size, price);
+              setSize(size);
             }}
           >
+            {handleSizeSelect(itemSize)}
             <p>{size}</p>
 
             <p className="size-price">D {viewItem.size[size]}</p>
