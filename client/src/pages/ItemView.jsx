de124eef15ef4f0afc07001aa18906c17f594e46
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-05 17:48:35 -0500

    Feat: shopping basket and handle submit

--- client/src/pages/ItemView.jsx
+++ client/src/pages/ItemView.jsx
@@ -1,6 +1,7 @@
 import { React, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useMediaQuery } from 'react-responsive';
+import Headroom from 'react-headroom';
 import Loader from '../components/Loader';
 import Image from '../components/Image';
 import Quantity from '../components/Quantity';
@@ -12,37 +13,57 @@ export default function ItemView() {
   const { id } = useParams();
   const { items, isLoading } = useFetchedItems();
 
-  const [selectedSize, setSelectedSize] = useState('');
-  const [selectedSizePrice, setSelectedSizePrice] = useState('');
+  let itemSize;
+  let itemPrice;
 
   const isBigScreen = useMediaQuery({ query: '(min-width: 1050px)' });
   const isSmallScreen = useMediaQuery({ query: '(max-width: 1050px)' });
 
   const item = items.find(item => item._id === id);
 
-  function handleSizeSelect(size, price) {
-    setSelectedSize(size);
-    setSelectedSizePrice(price);
+  function handleSizeSelect(size) {
+    itemSize = size;
+  }
+
+  function handleSizeSelectPrice(price) {
+    itemPrice = price;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(itemSize);
+    console.log(itemPrice);
   }
 
   return isLoading ? (
     <Loader />
   ) : (
     <div className="view-container">
-      <a href="/" className="back-btn-container">
-        <span className="material-symbols-outlined">arrow_back</span>
-        <>Back</>
-      </a>
+      <Headroom>
+        <div className="item-view-navbar">
+          <div className="back-button">
+            <a href="/" className="back-btn-container">
+              <span className="material-symbols-outlined">arrow_back</span>
+              <>Back</>
+            </a>
+          </div>
+
+          <div className="shopping-bag-container">
+            <a>
+              <span className="items-number">1</span>
+              <span className="material-symbols-outlined">shopping_bag</span>
+            </a>
+          </div>
+        </div>
+      </Headroom>
 
       <div className="item-view-container">
-        <div className="item-view-card">
+        <form className="item-view-card" onSubmit={handleSubmit}>
           <div className="item-view-image-container">
             {isBigScreen && (
               <div className="header-price-container">
                 <p className="item-view-card-header">{item.name}</p>
-                <p className="item-price">
-                  D {selectedSize ? `${selectedSizePrice}` : `${item.price}`}
-                </p>
+                <p className="item-price">D {item.price}</p>
               </div>
             )}
 
@@ -55,9 +76,7 @@ export default function ItemView() {
             {isSmallScreen && (
               <div className="header-price-container">
                 <p className="item-view-card-header">{item.name}</p>
-                <p className="item-price">
-                  D {selectedSize ? `${selectedSizePrice}` : `${item.price}`}
-                </p>
+                <p className="item-price">D {item.price}</p>
               </div>
             )}
           </div>
@@ -76,7 +95,11 @@ export default function ItemView() {
 
               <span className="divider"></span>
 
-              <Size item={item} handleSizeSelect={handleSizeSelect} />
+              <Size
+                item={item}
+                handleSizeSelect={handleSizeSelect}
+                handleSizeSelectPrice={handleSizeSelectPrice}
+              />
 
               <span className="divider"></span>
 
@@ -93,13 +116,13 @@ export default function ItemView() {
                 className="additional-instructions"
               ></textarea>
 
-              <button className="add-item-btn">
+              <button className="add-item-btn" type="submit">
                 Add to cart
                 <span className="material-symbols-outlined">shopping_cart</span>
               </button>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
