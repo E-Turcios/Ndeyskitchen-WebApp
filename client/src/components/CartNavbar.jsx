69927c595bd4f8209ec651e1c30d0b6d673e0170
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-06 20:21:48 -0500

    Feat: Headroom added

--- client/src/components/CartNavbar.jsx
+++ client/src/components/CartNavbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import Headroom from 'react-headroom';
 
 export default function CartNavbar() {
   return (
-    <div className="item-view-navbar">
-      <div className="home-button">
-        <a href="/" className="back-btn-container">
-          <>Home</>
-        </a>
+    <Headroom>
+      <div className="item-view-navbar">
+        <div className="home-button">
+          <a href="/" className="back-btn-container">
+            <>Home</>
+          </a>
+        </div>
       </div>
-    </div>
+    </Headroom>
   );
 }
