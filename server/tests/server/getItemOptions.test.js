a7e47648a08efb1e44e50659371274f6eedde24f
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-07 19:43:51 -0500

    Feat: getting time options done

---
+++ server/tests/server/getItemOptions.test.js
@@ -0,0 +1,10 @@
+const app = require('../../index');
+const supertest = require('supertest');
+
+describe('Items options fetching', () => {
+  it('Successfull items options fetching', async () => {
+    const response = await supertest(app).get('/api/items/get-item-options');
+
+    expect(response.statusCode).toBe(200);
+  });
+});
