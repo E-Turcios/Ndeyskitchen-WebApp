503d0bd9f6ec1ebba85e4484408ea0cb248dce67
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-10 21:07:29 -0500

    Feat: order creation

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -6,18 +6,7 @@ const { sendEmail } = require('../sendEmail');
 const { ORDER_VALIDATION_FAILED, ORDER_PLACED } = require('../messages');
 
 async function createOrder(req, res) {
-  const {
-    firstName,
-    lastName,
-    email,
-    number,
-    residence,
-    service,
-    paymentMethod,
-    total,
-    cart,
-    datesAndTimes,
-  } = req.body;
+  const { information } = req.body;
 
   try {
     const counter = await Counter.findOneAndUpdate(
@@ -28,19 +17,19 @@ async function createOrder(req, res) {
 
     const order = await Order.create({
       orderNumber: counter.value.toString().padStart(5, '0'),
-      firstName,
-      lastName,
-      email,
-      number,
-      residence,
-      total,
-      service,
-      paymentMethod,
-      nonCakesDate: datesAndTimes.nonCake.selectedDate,
-      nonCakesTime: datesAndTimes.nonCake.selectedTime,
-      cakesDate: datesAndTimes.cake.selectedDate,
-      cakesTime: datesAndTimes.cake.selectedTime,
-      items: cart,
+      firstName: information.firstName,
+      lastName: information.lastName,
+      email: information.email,
+      number: information.number,
+      residence: information.residence,
+      total: information.total,
+      service: information.service,
+      paymentMethod: information.paymentMethod,
+      nonCakesDate: information.datesAndTimes.nonCake.selectedDate,
+      nonCakesTime: information.datesAndTimes.nonCake.selectedTime,
+      cakesDate: information.datesAndTimes.cake.selectedDate,
+      cakesTime: information.datesAndTimes.cake.selectedTime,
+      items: information.items,
     });
 
     if (order) return res.status(200).json({ Message: ORDER_PLACED });
