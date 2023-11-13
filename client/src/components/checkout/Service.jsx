c340d21ba8064a71db5b104b9ee1dde05a32fedc
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-13 17:03:18 -0500

    Feat: text added to service

--- client/src/components/checkout/Service.jsx
+++ client/src/components/checkout/Service.jsx
@@ -9,24 +9,29 @@ export default function Service({ onButtonClick }) {
     onButtonClick(id);
   }
   return (
-    <div className="service-container">
-      <p className="section">Service</p>
-      <div className="button-container">
-        <button
-          className={buttonId === 'Pick-up' ? 'clicked-btn' : 'btn'}
-          onClick={event => handleServiceButtonClick(event, 'Pick-up')}
-        >
-          Pick up
-        </button>
-        <button
-          className={buttonId === 'Delivery' ? 'clicked-btn' : 'btn'}
-          onClick={event => {
-            handleServiceButtonClick(event, 'Delivery');
-          }}
-        >
-          Delivery
-        </button>
+    <div className="service-message-container">
+      <div className="service-container">
+        <p className="section">Service</p>
+        <div className="button-container">
+          <button
+            className={buttonId === 'Pick-up' ? 'clicked-btn' : 'btn'}
+            onClick={event => handleServiceButtonClick(event, 'Pick-up')}
+          >
+            Pick up
+          </button>
+          <button
+            className={buttonId === 'Delivery' ? 'clicked-btn' : 'btn'}
+            onClick={event => {
+              handleServiceButtonClick(event, 'Delivery');
+            }}
+          >
+            Delivery
+          </button>
+        </div>
       </div>
+      {buttonId === 'Delivery' && (
+        <p className="delivery-fees">Driver's drop-off, fees apply</p>
+      )}
     </div>
   );
 }
