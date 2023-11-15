ff2aa18c861f5c04e828d092b9879cba7780c9bb
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-14 19:56:23 -0500

    Feat: userUpdateToken verification custom hook

---
+++ client/src/hooks/useUserUpdateInfoTokenVerifier.js
@@ -0,0 +1,28 @@
+import { React, useState, useEffect } from 'react';
+
+export default function useUserUpdateInfoTokenVerifier() {
+  const [token, setToken] = useState(false);
+
+  const userUpdateInfoToken = localStorage.getItem('userUpdateInfoToken');
+
+  useEffect(() => {
+    async function verifyUserUpdateInfoToken() {
+      const response = await fetch('/api/users/verify-user-update-token', {
+        method: 'POST',
+        body: JSON.stringify({ userUpdateInfoToken }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+
+      const json = await response.json();
+
+      if (!response.ok) {
+        console.log(json.Message);
+        return;
+      }
+
+      setToken(true);
+    }
+    verifyUserUpdateInfoToken();
+  }, []);
+  return { token };
+}
