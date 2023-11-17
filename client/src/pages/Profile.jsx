2b049d0a8e21cbcdff5e3ea72c27ffbdefecbd35
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-17 18:55:24 -0500

    Feat: imports modified

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { React } from 'react';
 import Loader from '../components/Loader';
-import ProfilePageNavbar from '../components/profile/ProfilePageNavbar';
-import ProfilePageContent from '../components/profile/ProfilePageContent';
+import ProfilePageNavbar from '../components/Profile/ProfilePageNavbar';
+import ProfilePageContent from '../components/Profile/ProfilePageContent';
 import useFetchedUserData from '../hooks/useFetchedUserData';
 
 export default function Profile() {
