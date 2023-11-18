3e48878cc1c0ad1643aecb4ff8b0bef706fd2117
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-18 18:57:15 -0500

    Feat: ordes page

--- client/src/components/orders/OrdersPageContent.jsx
+++ client/src/components/orders/OrdersPageContent.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import truncateComponentList from '../../scripts/truncateComponentList';
 
 export default function OrdersPageContent({ orders }) {
-  console.log(orders);
+  //console.log(orders);
   return (
     <div className="profile-content-container orders-content-container">
       <p className="cart-header profile-content-header orders-content-header">
@@ -13,15 +14,24 @@ export default function OrdersPageContent({ orders }) {
             <p className="order-card-date">
               {new Date(order.createdAt).toLocaleDateString()}
             </p>
-            <p className="order-card-name">{`${order.firstName} ${order.lastName}`}</p>
+            <p className="order-card-user-name">{`${order.firstName} ${order.lastName}`}</p>
 
-            <div className="items-names">
-              {order.items.map(item => (
-                <p className="order-card-items-name" key={item.id}>
-                  {item.name}
+            {order.items.map(item => (
+              <div className="items-names-components" key={item.id}>
+                <div className="name-price-container">
+                  <p className="order-card-item-name">{item.name}</p>
+                  <p className="price-quantity-tag">{`${item.quantity} x D ${item.price}`}</p>
+                </div>
+
+                <p className="order-card-item-components">
+                  {truncateComponentList(item.components, 55)}
                 </p>
-              ))}
-            </div>
+              </div>
+            ))}
+
+            <p className="order-total">{`Total: D ${order.total}`}</p>
+            <button>Reorder</button>
+            <span className="divider"></span>
           </div>
         ))}
       </div>
