1488579eaad3d432226dfd7bb3d66334158fa231
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-17 20:20:02 -0500

    Feat: orders page

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import ProfileAndOrderPagesNavbar from '../components/ProfileAndOrderPagesNavbar';
+import OrdersPageContent from '../components/orders/OrdersPageContent';
 
 export default function Orders() {
-  return <div>Order</div>;
+  return (
+    <div className="orders-page-container">
+      <ProfileAndOrderPagesNavbar />
+      <OrdersPageContent />
+    </div>
+  );
 }
