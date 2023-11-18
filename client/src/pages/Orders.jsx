2df898b243a9e7ae282442a10af2c00452b1b0cc
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-17 20:54:56 -0500

    Feat: orders passed as argument

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ProfileAndOrderPagesNavbar from '../components/ProfileAndOrderPagesNavbar';
 import OrdersPageContent from '../components/orders/OrdersPageContent';
+import Loader from '../components/Loader';
+import useFetchedUserOrders from '../hooks/useFetchedUserOrders';
 
 export default function Orders() {
-  return (
+  const { orders, isLoading } = useFetchedUserOrders();
+  return isLoading ? (
+    <Loader />
+  ) : (
     <div className="orders-page-container">
       <ProfileAndOrderPagesNavbar />
-      <OrdersPageContent />
+      <OrdersPageContent orders={orders} />
     </div>
   );
 }
