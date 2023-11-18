1405fb913487e3b70ca6ac8fc50db561125aa398
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-17 20:16:27 -0500

    Feat: users orders fetching custom hook

---
+++ client/src/hooks/useFetchedUserOrders.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import useAuthContext from './useAuthContext';
+
+export default function useFetchedUserOrders() {
+  const [orders, setOrders] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { user } = useAuthContext();
+
+  useEffect(() => {
+    async function getOrders() {
+      if (!user) return;
+
+      const response = await fetch('/api/orders/get-user-orders', {
+        method: 'POST',
+        body: JSON.stringify({ user }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const json = await response.json();
+
+      if (!response.ok) console.log(json.Message);
+
+      setOrders(json);
+      setIsLoading(false);
+    }
+    getOrders();
+  }, []);
+  return { orders, isLoading };
+}
