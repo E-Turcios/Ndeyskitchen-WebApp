28163d6d4f1b9f915c8d24331207b9d0cd0f2533
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-17 20:17:49 -0500

    Feat: egt user's orders route set up

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
-const { validateOrder } = require('../middlewares');
+const { validateOrder, validateUser } = require('../middlewares');
 
 router.get('/');
 
 router.post('/', validateOrder, orderController.createOrder);
+
+router.post('/get-user-orders', validateUser, orderController.getUserOrders);
 
 module.exports = router;
