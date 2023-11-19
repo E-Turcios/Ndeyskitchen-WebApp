7c2ba94412ffe4130cf7e222aaab6d64d7d0ca76
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-18 19:55:38 -0500

    Feat: orders page done

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import ProfileAndOrderPagesNavbar from '../components/ProfileAndOrderPagesNavbar';
 import OrdersPageContent from '../components/orders/OrdersPageContent';
-import Loader from '../components/Loader';
 import useFetchedUserOrders from '../hooks/useFetchedUserOrders';
 
 export default function Orders() {
   const { orders, isLoading } = useFetchedUserOrders();
 
-  return isLoading ? (
-    <Loader />
-  ) : (
+  return (
     <div className="orders-page-container">
       <ProfileAndOrderPagesNavbar />
-      <OrdersPageContent orders={orders} />
+      <OrdersPageContent orders={orders} isLoading={isLoading} />
     </div>
   );
 }
