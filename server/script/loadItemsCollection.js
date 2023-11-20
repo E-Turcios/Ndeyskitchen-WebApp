3817799d5d2948c02be5fadedc15e3717bf08c06
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-11-19 19:28:38 -0500

    Feat: New items added

--- server/script/loadItemsCollection.js
+++ server/script/loadItemsCollection.js
@@ -3,42 +3,244 @@ const mongoose = require('mongoose');
 const chalk = require('chalk');
 
 const image = {
+  alfredoShrimpPasta: 'items/alfredo-shrimp-pasta.jpg',
+  barbieCake: 'items/barbie-cake.jpg',
   biscoffCake: 'items/biscoff-cake.jpg',
+  breadedFriedShrimp: 'items/breaded-fried-shrimp.jpg',
   breakFastBox: 'items/breakfast-box.jpg',
-  chocolateOverload: 'items/chocolate-overload.jpg',
+  cakeSlice: 'items/cake-slice.jpg',
+  chickenAfra: 'items/chicken-afra.jpg',
+  chickenBatbouts: 'items/chicken-batbouts.jpg',
+  chocolateOverloadCake: 'items/chocolate-overload-cake.jpg',
   cookies: 'items/cookies.jpg',
+  cupcakes: 'items/cupcakes.jpg',
+  darkThemedCake: 'items/dark-themed-cake.jpg',
+  donuts: 'items/donuts.jpg',
+  ebbeh: 'items/ebbeh.jpg',
+  fingerFoodTray: 'items/finger-food-tray.jpg',
+  friedChicken: 'items/fried-chicken.jpg',
   fruitPlatter: 'items/fruit-platter.jpg',
-  snacks: 'items/snacks.jpg',
+  grilledChickenPotatoes: 'items/grilled-chicken-potatoes.jpg',
+  pinkFloralCake1: 'items/pink-floral-cake-1.jpg',
+  pinkFloralCake2: 'items/pink-floral-cake-2.jpg',
+  pinkFloralCake3: 'items/pink-floral-cake-3.jpg',
+  pizza: 'items/pizza.jpg',
+  purpleFloralCake: 'items/purple-floral-cake.jpg',
+  redVelvetCake: 'items/red-velvet-cake.jpg',
+  savoryDelights: 'items/savory-delights.jpg',
+  snackBowl: 'items/snack-bowl.jpg',
+  squareCake: 'items/square-cake.jpg',
+  stirFriedNoodles: 'items/stir-fried-noodles.jpg',
+  superheroCake: 'items/superhero-cake.jpg',
   weddingCake: 'items/wedding-cake.jpg',
-  purpleFloral: 'items/purple-floral-cake.jpg',
-  pinkFloral: 'items/pink-floral-cake.jpg',
+  weddingCake2: 'items/wedding-cake-2.jpg',
+  whiteFloralCake: 'items/white-floral-cake.jpg',
+  whitePrincessThemedCake: 'items/white-princess-cake.jpg',
 };
 
 const filter = {
-  ALL: 'All',
   CAKES: 'Cakes',
   CUPCAKES: 'Cupcakes',
   COOKIES: 'Cookies',
-  FRUIT_PLATTER: 'Fruit Platter',
   DESSERT_CUPS: 'Dessert Cups',
   BREAK_FAST_BOX: 'Breakfast Box',
   SNACKS: 'Snacks',
+  PASTA: 'Pasta',
+  SHRIMPS: 'Shrimps',
+  SOUPS: 'Soups',
+  CHICKEN: 'Chicken',
+  PIZZA: 'Pizza',
+  NOODLES: 'Noodles',
+  FRUITS: 'Fruits',
+  DONUTS: 'Donuts',
 };
 
 const itemsData = [
   {
-    name: '2-Tier Wedding Cake',
+    name: '2 Tiers Wedding Cake',
     category: 'Sweet',
     filter: filter.CAKES,
     flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
     components:
-      'Vanilla + Red Velvet Layers, Strawberry + Cream Cheese Filling',
+      'Vanilla And Red Velvet Layers, Strawberry and Cream Cheese Filling',
     price: 9000,
     size: {
       Large: '9000',
     },
     image: image.weddingCake,
-    alt: '2-Tier Wedding Cake',
+    alt: 'Wedding Cake',
+  },
+
+  {
+    name: 'Stir Fried Noodles',
+    category: 'Savory',
+    filter: filter.NOODLES,
+    components: 'Onions, Protein, ',
+    price: 350,
+    size: {
+      Beef: '350',
+      Chicken: '350',
+      Shrimp: '350',
+      Mixed: '450',
+    },
+    image: image.stirFriedNoodles,
+    alt: 'Stir Fried Noodles',
+  },
+
+  {
+    name: '3 Tiers Wedding Cake',
+    category: 'Sweet',
+    filter: filter.CAKES,
+    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
+    components: 'Flavor Layers, Strawberry and Cream Cheese Filling',
+    price: 14000,
+    size: {
+      Large: '14000',
+    },
+    image: image.weddingCake2,
+    alt: 'Wedding Cake',
+  },
+
+  {
+    name: 'Fried Chicken',
+    category: 'Savory',
+    filter: filter.CHICKEN,
+    components: 'Chicken, Fries, Ketchup',
+    price: 350,
+    size: {
+      '3Pcs': '350',
+      '6Pcs': '500',
+    },
+    image: image.friedChicken,
+    alt: 'Fried Chicken',
+  },
+
+  {
+    name: 'Donuts',
+    category: 'Sweet',
+    filter: filter.DONUTS,
+    components: 'To be filled',
+    price: 300,
+    size: {
+      '3Pcs': '300',
+      '12Pcs': '1500',
+    },
+    image: image.donuts,
+    alt: 'Donuts',
+  },
+
+  {
+    name: 'Chicken Afra',
+    category: 'Savory',
+    filter: filter.CHICKEN,
+    components: 'To be filled',
+    price: 300,
+    size: {
+      '1 Dish': '300',
+    },
+    image: image.chickenAfra,
+    alt: 'Chicken Afra',
+  },
+
+  {
+    name: 'Breaded Fried Shrimps',
+    category: 'Savory',
+    filter: filter.SHRIMPS,
+    components: 'Shrimps, Fries, Ketchup',
+    price: 350,
+    size: {
+      Medium: '400',
+      Large: '600',
+    },
+    image: image.breadedFriedShrimp,
+    alt: 'Breaded Fried Shrimps',
+  },
+
+  {
+    name: 'Alfredo Pasta',
+    category: 'Savory',
+    filter: filter.PASTA,
+    components: 'Creamy Alfredo Sauce, Protein',
+    price: 350,
+    size: {
+      Chicken: '450',
+      Beef: '450',
+      Shrimp: '450',
+    },
+    image: image.alfredoShrimpPasta,
+    alt: 'Alfredo Pasta',
+  },
+
+  {
+    name: 'Pizza',
+    category: 'Savory',
+    filter: filter.PIZZA,
+    flavors: ['Beef', 'Chicken', 'Shrimp'],
+    components: 'Tomato Sauce, Green Pepper, Tomato',
+    price: 350,
+    size: {
+      Medium: '400',
+      Large: 'D600',
+    },
+    image: image.pizza,
+    alt: 'Pizza',
+  },
+
+  {
+    name: 'Square Cake',
+    category: 'Sweet',
+    filter: filter.CAKES,
+    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
+    components: 'White Chocolate Buttercream',
+    price: 5500,
+    size: {
+      Small: '4500',
+      Medium: '4500',
+      Large: '6500',
+    },
+    image: image.squareCake,
+    alt: 'Square Cake',
+  },
+
+  {
+    name: 'Red Velvet Cake',
+    category: 'Sweet',
+    filter: filter.CAKES,
+    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
+    components: 'Gold Sheets, Candy Pearls',
+    price: 3000,
+    size: {
+      Small: '3000',
+    },
+    image: image.redVelvetCake,
+    alt: 'Red Velvet Cake',
+  },
+
+  {
+    name: 'Savory Delights',
+    category: 'Savory',
+    filter: filter.SNACKS,
+    components: 'Chicken Leg, Shrimp Spring Roll, Nems, Beef Bun',
+    price: 200,
+    size: {
+      '1 Pack': '200',
+    },
+    image: image.savoryDelights,
+    alt: 'Savory Delights',
+  },
+
+  {
+    name: 'Snack Bowl',
+    category: 'Savory',
+    filter: filter.SNACKS,
+    components:
+      'Mini Burger, Chicken Batbout, Hot Dog, Beef Bun, Meat Pie, Beef Wrap',
+    price: 500,
+    size: {
+      '1 Bowl': '500',
+    },
+    image: image.snackBowl,
+    alt: 'Savory Delights',
   },
 
   {
@@ -47,19 +249,19 @@ const itemsData = [
     filter: filter.CAKES,
     flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
     components: 'Chocolate Ganache, White Chocolate Buttercream',
-    price: 6500,
+    price: 5000,
     size: {
-      Medium: '6500',
-      Large: '7000',
+      Medium: '5000',
+      Large: '6000',
     },
-    image: image.chocolateOverload,
+    image: image.chocolateOverloadCake,
     alt: 'Chocolate Overload Cake',
   },
 
   {
     name: 'Fruit Platter',
     category: 'Sweet',
-    filter: filter.FRUIT_PLATTER,
+    filter: filter.FRUITS,
     components: 'Watermelon, Pineapple, Orange, Mandarine, Grapes',
     price: 2500,
     size: {
@@ -70,7 +272,21 @@ const itemsData = [
   },
 
   {
-    name: '12 Chocolate Cupcakes',
+    name: 'White Floral Cake',
+    category: 'Sweet',
+    filter: filter.CAKES,
+    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
+    components: 'Chocolate icing',
+    price: 3500,
+    size: {
+      Small: '3500',
+    },
+    image: image.whiteFloralCake,
+    alt: 'Barbie Cake',
+  },
+
+  {
+    name: 'Chocolate Cupcakes',
     category: 'Sweet',
     filter: filter.CUPCAKES,
     components: 'White Chocolate Buttercream',
@@ -78,15 +294,43 @@ const itemsData = [
     size: {
       '12Pcs': '2000',
     },
-    image: image.pinkFloral,
-    alt: '12 Chocolate Cupcakes',
+    image: image.cupcakes,
+    alt: 'Chocolate Cupcakes',
+  },
+
+  {
+    name: 'Barbie Cake',
+    category: 'Sweet',
+    filter: filter.CAKES,
+    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
+    components: 'Chocolate icing',
+    price: 4500,
+    size: {
+      Medium: '4500',
+      Large: '5500',
+    },
+    image: image.barbieCake,
+    alt: 'Barbie Cake',
+  },
+
+  {
+    name: 'Chicken Batbout',
+    category: 'Savory',
+    filter: filter.CHICKEN,
+    components: 'Chicken, Lettuce, Cheese, Special Sauce',
+    price: 500,
+    size: {
+      '1 Dish': '450',
+    },
+    image: image.chickenBatbouts,
+    alt: 'Savory Delights',
   },
 
   {
     name: 'Sugar Cookies',
     category: 'Sweet',
     filter: filter.COOKIES,
-    components: '25 Sugar Cookies',
+    components: 'Sugar Cookies',
     price: 2500,
     size: {
       '25Pcs': '2500',
@@ -102,9 +346,10 @@ const itemsData = [
     flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
     components:
       'Vanilla Cake, White Chocolate Biscoff Buttercream, Biscoff Spread',
-    price: 5000,
+    price: 4500,
     size: {
-      Medium: '5000',
+      Medium: '4500',
+      Large: '5500',
     },
     image: image.biscoffCake,
     alt: 'Biscoff Drip Cake',
@@ -120,12 +365,42 @@ const itemsData = [
     size: {
       '12Pcs': '2500',
     },
-    image: image.purpleFloral,
+    image: image.purpleFloralCake,
     alt: 'Cheesecake Dessert Cups',
   },
 
   {
-    name: 'Finger Licking Snacks',
+    name: 'Pink Floral Cake',
+    category: 'Sweet',
+    filter: filter.CAKES,
+    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
+    components: 'Chocolate icing',
+    price: 4500,
+    size: {
+      Medium: '4500',
+      Large: '5500',
+    },
+    image: image.pinkFloralCake3,
+    alt: 'Pink Floral Cake',
+  },
+
+  {
+    name: 'Superhero Themed Cake',
+    category: 'Sweet',
+    filter: filter.CAKES,
+    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
+    components: 'Flavor of Choice',
+    price: 4500,
+    size: {
+      Medium: '4500',
+      Large: '5500',
+    },
+    image: image.superheroCake,
+    alt: 'Superhero Themed Cake',
+  },
+
+  {
+    name: 'Finger Food Tray',
     category: 'Savory',
     filter: filter.SNACKS,
     components:
@@ -134,8 +409,37 @@ const itemsData = [
     size: {
       '25Pcs': '3000',
     },
-    image: image.snacks,
-    alt: 'Finger Licking Snacks',
+    image: image.fingerFoodTray,
+    alt: 'Finger Food Tray',
+  },
+
+  {
+    name: 'Dark Themed Cake',
+    category: 'Sweet',
+    filter: filter.CAKES,
+    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
+    components: 'Chocolate icing',
+    price: 4500,
+    size: {
+      Medium: '4500',
+      Large: '5500',
+    },
+    image: image.darkThemedCake,
+    alt: 'Dark Themed Cake',
+  },
+
+  {
+    name: 'Sliced Cake',
+    category: 'Sweet',
+    filter: filter.CAKES,
+    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
+    components: 'To be filled',
+    price: 400,
+    size: {
+      '1 Slice': '400',
+    },
+    image: image.cakeSlice,
+    alt: 'Sliced Cake',
   },
 
   {
@@ -153,31 +457,76 @@ const itemsData = [
   },
 
   {
-    name: 'Purple Floral Design Cake',
-    category: 'Sweet',
-    filter: filter.CAKES,
-    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
-    components: 'Flavor of Choice',
-    price: 5000,
-    size: {
-      Medium: '5000',
-    },
-    image: image.purpleFloral,
-    alt: 'Purple Floral Design Cake',
-  },
-
-  {
-    name: 'Pink Floral Chocolate Cake',
+    name: 'Princess Themed Cake',
     category: 'Sweet',
     filter: filter.CAKES,
     flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
     components: 'Chocolate icing',
-    price: 5000,
+    price: 4500,
     size: {
-      Medium: '5000',
+      Medium: '4500',
+      Large: '5500',
     },
-    image: image.pinkFloral,
-    alt: 'Pink Floral Chocolate Cake',
+    image: image.whitePrincessThemedCake,
+    alt: 'Princess Themed Cake',
+  },
+
+  {
+    name: 'Pink Floral Cake',
+    category: 'Sweet',
+    filter: filter.CAKES,
+    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
+    components: 'Chocolate icing',
+    price: 4500,
+    size: {
+      Medium: '4500',
+      Large: '5500',
+    },
+    image: image.pinkFloralCake2,
+    alt: 'Pink Floral Cake',
+  },
+
+  {
+    name: 'Chicken And Mashed Potatoes',
+    category: 'Savory',
+    filter: filter.CHICKEN,
+    components: 'Grilled Chicken, Mashed Poatates, Barbecue Sauce',
+    price: 375,
+    size: {
+      '1 Dish': '375',
+    },
+    image: image.grilledChickenPotatoes,
+    alt: 'Chicken And Mashed Potatoes',
+  },
+
+  {
+    name: 'Purple Floral Cake',
+    category: 'Sweet',
+    filter: filter.CAKES,
+    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
+    components: 'Flavor of Choice',
+    price: 4500,
+    size: {
+      Medium: '4500',
+      Large: '5500',
+    },
+    image: image.purpleFloralCake,
+    alt: 'Purple Floral Design Cake',
+  },
+
+  {
+    name: 'Pink Floral Cake',
+    category: 'Sweet',
+    filter: filter.CAKES,
+    flavors: ['Chocolate', 'Vanilla', 'Red Velvet'],
+    components: 'Chocolate icing',
+    price: 4500,
+    size: {
+      Medium: '4500',
+      Large: '5500',
+    },
+    image: image.pinkFloralCake1,
+    alt: 'Pink Floral Cake',
   },
 ];
 
