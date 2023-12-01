3320d941685089eb71e04b4e78c1e22d5164b653
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-01 02:34:17 -0500

    Feat: email typo fixed

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -95,7 +95,7 @@ async function sendEmailToUserAndAdmin(information, orderNumber) {
       <p style="font-size: 16px; text-align: center;">Total: D ${information.total}</p>
       <p style="font-size: 16px;">Payment Method: ${information.paymentMethod}</p>
       <p style="font-size: 16px;">For any inquiries, please reach out to us:</p>
-      <a style="font-size: 16px; margin: 0;" href="mailto:support@ndeyskicthen.com">upport@ndeyskicthen.com</a>
+      <a style="font-size: 16px; margin: 0;" href="mailto:support@ndeyskicthen.com">support@ndeyskicthen.com</a>
       <p style="font-size: 16px; margin: 0;">WhatsApp: +220 794 4636</p>
       <p style="font-size: 16px;">We appreciate your business!</p>
     </div>
