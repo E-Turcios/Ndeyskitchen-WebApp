9d76d7a3f9f5750b81e962c9e597bea3d31fd135
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-04 16:57:40 -0500

    Feat: Model updated

--- server/database/models/userModel.js
+++ server/database/models/userModel.js
@@ -8,8 +8,8 @@ const UserSchema = new Schema(
     lastName: { type: String, required: true, minlength: 2, maxlength: 50 },
     email: { type: String, required: true, unique: true, maxlength: 100 },
     password: { type: String, default: 'N/A', maxlength: 100 },
-    number: { type: String, default: 'N/A', maxlength: 20 },
-    countryCode: { type: String, default: 'N/A', maxlength: 4 },
+    number: { type: String, unique: true, sparse: true, maxlength: 20 },
+    countryCode: { type: String, maxlength: 4 },
     sub: {
       type: String,
       required: false,
@@ -17,8 +17,7 @@ const UserSchema = new Schema(
       maxlength: 100,
       sparse: true,
     },
-    residence: { type: String, default: 'N/A', maxlength: 50 },
-    isGoogleUser: { type: Boolean, default: false, maxlength: 5 },
+    residence: { type: String, maxlength: 50 },
     token: { type: String, default: 'N/A', maxlength: 100 },
   },
   { timestamps: true }
