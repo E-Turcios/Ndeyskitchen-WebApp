938639c7b9ef1f65c6d9fecdcdb5f24ab93b089f
boundy99 <abdoulayeboundydjikine@gmail.com>
2023-12-08 22:34:51 -0500

    Feat: Image lazy loading

--- client/src/components/Image.jsx
+++ client/src/components/Image.jsx
@@ -1,6 +1,7 @@
 export default function Image(props) {
   return (
     <img
+      loading="lazy"
       className={props.className}
       alt={props.alt}
       src={require('../images/' + props.src)}
